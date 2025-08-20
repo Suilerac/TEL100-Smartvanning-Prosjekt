34bfe00ee9ebfc661323c6b453859c5a755fdf7c
JohannesStandal <johanneshusevagstandal@gmail.com>
2025-08-20 13:31:36 +0200

    Kildekode for dashbord
    Inkluderer html, css, og javascript for nettstaden. Må oppdaterast for å kommunisere med Arduino

---
+++ TEL100 - Dashbord/index.js
@@ -0,0 +1,74 @@
+const waterLevelElement = document.getElementById("vannBestand")
+
+const statusElement1 = document.getElementById("status1")
+const statusElement2 = document.getElementById("status2")
+const statusElement3 = document.getElementById("status3")
+
+const indoorPlantCheckbox = document.getElementById("indoor")
+
+const lonInput = document.getElementById("lon")
+const latInput = document.getElementById("lat")
+const moistInput = document.getElementById("moist")
+
+//Desse fire variablane må lesast av arduino
+var targetMoistLevel; //integer
+var indoorPlant; //booleansk verdi
+var langditude;
+var latitude;
+
+//Desse 2 variablane må sendast
+var measuredMoistLevel; //integer (0, 100) %
+var measuredWaterLevel = 10; //integer (0, 10)
+
+
+
+function RenderWaterTank(){
+    let percent = (measuredWaterLevel / 10) * 100; 
+    waterLevelElement.innerHTML = "Vannbestand - " + String(percent) + "%"
+    for (let i = 1; i <= 10; i++){
+        const waterBarElement = document.getElementById(String(i))
+
+        if (i <= measuredWaterLevel){
+            waterBarElement.style.visibility = "visible"
+        }
+        else {
+            waterBarElement.style.visibility = "hidden"
+        }
+    }
+}
+
+function Fetch(element){
+    let val = element.value
+    return JSON.parse(JSON.parse(val).toFixed(4))
+}
+
+//Les inputs og oppdater samsvarande variablar
+setInterval(()=>{
+    indoorPlant = indoorPlantCheckbox.checked
+    langditude = Fetch(lonInput)
+    latitude = Fetch(latInput)
+    targetMoistLevel = Fetch(moistInput)
+
+}, 100)
+
+setInterval(()=>{
+    measuredWaterLevel--
+    if (measuredWaterLevel < 0) measuredWaterLevel = 10
+    RenderWaterTank()
+},1000)
+
+
+/**
+Input:
+- posisjon (lengdegrad, breiddegrad) 4 desimalar 
+- innendørsplante: bool
+- ønska fuktighet: %
+
+Status:
+- Vannbestand i tank
+- current fuktighetsnivå
+- skal regne neste 12 timar
+
+text nederst
+-"Basert på data frå MET-Norge"
+**/
